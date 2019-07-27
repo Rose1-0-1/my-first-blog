9a37e133ba90049488b1f68051a0f736b55db74b
Rose <rose@rose.rose>
2019-07-27 14:32:38 -0400

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,15 @@ from .models import Post
 from django.http import HttpResponse
 from django.template import loader
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render (request, 'blog/post_list.html', {'posts':posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 # def post_list(request):
 #     print("Hi!")
--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print("Base dir is " + BASE_DIR)
+print("Static_root is " + STATIC_ROOT)
